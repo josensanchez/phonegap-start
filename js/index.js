949a18013e9e4e5ead5e01025612abb079696097
Jose Norberto SANCHEZ <josensanchez@gmail.com>
2013-11-09 13:54:08 -0300

    event listener

--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener('backbutton', this.onBackButton, false);
+        //document.addEventListener('backbutton', this.onBackButton, false);
     },
     // deviceready Event Handler
     //
