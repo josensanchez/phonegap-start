1a07ae9d82029f2867d5a73e4d8e4e769dd73846
Jose Norberto SANCHEZ <josensanchez@gmail.com>
2013-12-13 13:52:11 -0300

    agrego ver visita en el mapa

--- js/test.js
+++ js/test.js
@@ -5,7 +5,7 @@ function runTest(){
         //app.timerId = setInterval(app.updateState,5000);
 
                 var oldVisitas = []; 
-                app.visitas = [{"id_visita":"1", "direccion": "Av. Cordoba 3000", "lat":-34.591671723881845, "lng":-58.43439102172852, "observaciones": "Hay que cambiar un gomin", "telefono_cliente": "+5411-5555-5555", "nombre_cliente": "Juan Clien"},{"id_visita":"2", "direccion": "Luis Maria Campos y maure", "lat":-34.56856352695369, "lng":-58.437438011169434, "observaciones": "dfsdf", "telefono_cliente": "+5411-5555-5555", "nombre_cliente": "Juan Clien"}];
+                app.visitas = [{"id_visita":"4", "direccion": "gaona 2334", "lat":-34.60783304208913, "lng":-58.44672918319702, "observaciones": "hay que cambiarle el gomín a la bici", "telefono_cliente": "+5411-5555-5555", "nombre_cliente": "Juan Clien", "estacion":{ "nombre":"","lng":0, "lat":0}},{"id_visita":"6", "direccion": "Sanchez de Bustamante 2334", "lat":-34.58697287092901, "lng":-58.40525150299072, "observaciones": "NO tiene interne en el celu", "telefono_cliente": "134566778889", "nombre_cliente": "pepe", "estacion":{ "nombre":"Agüero","lng":-58.40697884559631, "lat":-34.5916849721517}}];
                 var visitas_html = '<ul data-role="listview" data-filter="true" data-filter-placeholder="Buscar Visitas..." data-count-theme="b" data-inset="true">';
                 for(var i = 0; i < app.visitas.length; i++){
                     visita = app.visitas[i];
